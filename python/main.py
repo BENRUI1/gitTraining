072ca2693eaa6717faf8ad355582ddf228b1e871
BENRUI1 <nathalie.ruidant@atradius.com>
2021-04-14 09:54:16 +0200

    change user 1 branch 1

--- python/main.py
+++ python/main.py
@@ -13,6 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Nathalie')
-	print('Edit from user 2')
+    print("Edit from user 2")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
